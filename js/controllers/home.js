efd93761b3798be6e9315a32f9293c38374c8f3f
Siddharth <siddharth_1004@live.com>
2016-12-15 11:34:32 +0530

    Removed Angualr

--- js/controllers/home.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('portfolio')
-.controller('homePage', function($scope){
-	var mainImage = "/images/samplehome.jpg"
-	$scope.mainImage=mainImage;
-	console.log("CHECK");
-})
---
+++ js/navigationLoad.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#navigation').load('../index.html');
+});
